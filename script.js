95e8e320d99408335c3b6997253abea8990b10d3
Sergey <manofthetreasure@gmail.com>
2022-09-11 18:23:52 +0300

    m

---
+++ script.js
@@ -0,0 +1,24 @@
+const defaultBtn = document.querySelector("#default-btn");
+const customBtn = document.querySelector("#custom-btn");
+const fileName = document.querySelector(".file-name");
+
+
+const content = document.querySelector(".content");
+
+
+let regExp = /[0-9a-zA-Z\^\&\'\@\{\}\[\]\,\$\=\!\-\#\(\)\.\$\+\~\_]+$/;
+
+function active() {
+  defaultBtn.click();
+}
+
+defaultBtn.addEventListener("change", function () {
+  if (this.value) {
+    let nameValue = this.value.match(regExp);
+    fileName.style.display = "block";
+    fileName.textContent = nameValue;
+  } else {
+    fileName.style.display = "block";
+    fileName.textContent = "No file chosen";
+  }
+})
